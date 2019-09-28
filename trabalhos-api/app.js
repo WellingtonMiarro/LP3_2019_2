254532790033eff254612e407b99da14c4d3c74e
Wellington Miarro Ferreira <42249348+WellingtonMiarro@users.noreply.github.com>
2019-09-28 00:30:33 -0400

    Update app.js

--- trabalhos-api/app.js
+++ trabalhos-api/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//Middlewares
+
 const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -11,14 +11,11 @@ const app = express();
 
 app.use(logger('dev'));
 
-/** A utilizar a função CORS() sem nenhum argumento, voce estara dando liberando o acesso a todos os servicos para qualquer outra
-    aplicacao que tenha acesso a internet.*/
+
 app.use(cors());
 
-//Permite que nosso app Express consiga interpretar dados do frontend em formato JSON
 app.use(bodyParser.json());
-
-//Registra as rotas relacionadas a Listas 
+ 
 app.use('/trabalhos',rotasListas);
 
 module.exports = app;
